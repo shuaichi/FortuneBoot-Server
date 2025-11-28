3c9a6bb01ed860d86b29c11a98bf21593aacba91
zhangchi118 <zhangchi118@jd.com>
2025-11-28 14:41:52 +0800

    feat: 修复登录日志数据查看不到部分数据的问题

--- fortuneboot-infrastructure/src/main/java/com/fortuneboot/infrastructure/user/base/BaseLoginUser.java
+++ fortuneboot-infrastructure/src/main/java/com/fortuneboot/infrastructure/user/base/BaseLoginUser.java
@@ -39,7 +39,7 @@ public class BaseLoginUser implements UserDetails {
     /**
      * 登录信息
      */
-    protected final LoginInfo loginInfo = new LoginInfo();
+    protected LoginInfo loginInfo = new LoginInfo();
 
 
     public BaseLoginUser(Long userId, String username, String password) {
