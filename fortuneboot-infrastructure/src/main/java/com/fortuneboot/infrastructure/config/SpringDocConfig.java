000b5d0b0dae75d1cc4d571dccc1cf3666ac60d2
zhangchi118 <zhangchi118@jd.com>
2026-01-12 17:51:44 +0800

    feat: 版本号升级

--- fortuneboot-infrastructure/src/main/java/com/fortuneboot/infrastructure/config/SpringDocConfig.java
+++ fortuneboot-infrastructure/src/main/java/com/fortuneboot/infrastructure/config/SpringDocConfig.java
@@ -19,7 +19,7 @@ public class SpringDocConfig {
         return new OpenAPI()
                 .info(new Info().title("好记")
                         .description("好记记账管理系统演示")
-                        .version("v1.4.2")
+                        .version("v1.4.3")
                         .license(new License()
                                 .name("MIT 3.0")
                                 .url("https://demo.fortuneboot.com/")
